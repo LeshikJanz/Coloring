c4be06e0e487db2f61246f5d9420af90a52908b3
Alex Tereshko <treshaalesha@gmail.com>
2016-11-24 00:33:25 -0800

    Add redirect to any view

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,10 @@ module.exports = function(app){
         res.send(app.currentState);
     });
     app.get('/', require("./frontPage").get);
+    app.use(function(req,res, err){ //перенаправляет на любую страницу
+        var url = req.url.substring(1, req.url.length);
+        console.log(url);
+        if(err) res.status(404).send("Page not found");
+        else res.render(url);
+    })
 }
