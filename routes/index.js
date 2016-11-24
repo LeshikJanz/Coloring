be84687de2b699442f17265620086dfd4e906b83
Alex Tereshko <treshaalesha@gmail.com>
2016-11-24 03:21:10 -0800

    Fix bugs with create coping of svg

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ module.exports = function(app){
     app.get('/socket', require("./socket").get);
     app.post('/socket', require("./socket").post);
     app.post('/removeSession', require("./removeSession").post);
+    app.post('/createSvgFile', require("./createSvgFile").post);
     app.post('/currentState', function(req, res, err){
         console.log(app.currentState);
         res.send(app.currentState);
