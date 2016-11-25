a2aa10702d02f4991ff4c452afb3638ca2f74dda
Alex Tereshko <treshaalesha@gmail.com>
2016-11-25 03:01:12 -0800

    Add saving to vk

--- public/components/Button.js
+++ public/components/Button.js
@@ -1,30 +1,37 @@
 var myApp = myApp || {};
 
 myApp.Button = (function () {
-    function Button(id, hideId) {
-        console.log('Button');
+    function Button(id, containerId, logoId, shareBtn, socialCont) {
         var that = this;
         this.btn = document.querySelector(id);
-        this.hideDiv = document.querySelector(hideId);
+        this.container = document.querySelector(containerId);
+        this.logoId = document.querySelector(logoId);
+        this.shareBtn = document.querySelector(shareBtn);
+        this.social = document.querySelector(socialCont);
 
-        console.log(this.btn);
-        console.log(this.hideDiv);
 
-        // this.btn.addEventListener('click', function (e) {
-        //
-        //     var a = e.target;
-        //     alert(a);
-        //     console.log(a);
-        //     // that.hideDiv.classList.display = 'none';
-        // });
+        this.shareBtn.addEventListener('click', function () {
+            if (that.social.classList[1] == 'hide') {
+                that.social.classList.remove('hide');
+            } else {
+                that.social.classList.add('hide')
+            }
+        });
+
+        this.btn.addEventListener('click', function () {
+            that.container.classList.add('hide');
+            that.logoId.classList.remove('hide');
+        });
+
+        this.logoId.addEventListener('click', function () {
+            that.container.classList.remove('hide');
+            that.logoId.classList.add('hide');
+        })
     }
+
 
     Button.prototype.onClick = function (callBack) {
         this.onClickCallBack = callBack;
-    };
-
-    Button.prototype.hideDiv = function (){
-
     };
 
 
